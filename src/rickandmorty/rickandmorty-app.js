e308a16821fba8205307a65738319e5cb05252e3
rodrigorgv <rodrigo.emilianogv@gmail.com>
2025-02-02 17:27:14 -0600

    se finaliza ejercicio de http con api de rick y morty

--- src/rickandmorty/rickandmorty-app.js
+++ src/rickandmorty/rickandmorty-app.js
@@ -3,7 +3,7 @@
  */
 const fetchCharacter = async () => {
     //GENERA NUMERO ALEATORIO ENTRE 1 A 100 PARA CONSULTA
-    const numrandom = Math.round((Math.random() * 100) + 1);    
+    const numrandom = Math.round((Math.random() * 100) + 1);
     //HACE GET A API PARA MOSTRAR DATOS
     const res = await fetch(`https://rickandmortyapi.com/api/character/${numrandom}`)
     const data = await res.json();
@@ -17,7 +17,7 @@ const fetchCharacter = async () => {
  * @param {HTMLDivElement} element 
  */
 
-export const BreakingbadApp = async (element) =>{
+export const BreakingbadApp = async (element) => {
     document.querySelector('#app-title').innerHTML = "Rick and Morty App"
     element.innerHTML = 'Loading...';
 
@@ -25,16 +25,25 @@ export const BreakingbadApp = async (element) =>{
     //element.innerHTML = quote.name;
 
     const characterStatus = document.createElement('blockquote');
-    const  characterLabel = document.createElement('h3');
+    const characterLabel = document.createElement('h3');
     const nextCharacterButton = document.createElement('button');
     nextCharacterButton.innerHTML = 'next character '
-
+    
     const renderCharacter = (data) => {
         characterLabel.innerHTML = data.name;
         characterStatus.innerHTML = data.status;
-        element.replaceChildren(characterLabel, characterStatus, nextCharacterButton)   ;
+        element.replaceChildren(characterLabel, characterStatus, nextCharacterButton);
     }
     
     fetchCharacter()
+    .then(renderCharacter)
+    
+    const buscaPersonaje = () => {        
+        element.innerHTML = 'Loading...';
+        fetchCharacter()
         .then(renderCharacter)
+        
+    }
+    nextCharacterButton.addEventListener('click', buscaPersonaje)
+    
 }
